2e91991caaae779daa634cc1fa85b9ec6cbe8a86
Kelly Kemnitz <kellygkemnitz@gmail.com>
2024-11-27 08:16:35 -0600

    First commit

---
+++ recipes.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, render_template, redirect, url_for
+import mysql.connector
+
+app = Flask(__name__)
+
+db_config = {
+    'user': 'kellygkemnitz',
+    'password': '$h0ck3rg0ld',
+    'host': 'localhost',
+    'database': 'recipe_db'
+}
+
+@app.route('/')
+def index():
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("SELECT id, title, description FROM recipes")
+    recipes = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', recipes=recipes)
+
+@app.route('/recipe/<int:recipe_id>')
+def recipe(recipe_id):
+    conn = mysql.connector.connect(**db_config)
+    cursor = conn.cursor()
+    cursor.execute("SELECT title, description, instructions FROM recipes WHERE id = %s", (recipe_id,))
+    recipe = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return render_template('recipe.html', recipe=recipe)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_recipe():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        instructions = request.form['instructions']
+        conn = mysql.connector.connect(**db_config)
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO recipes (title, description, instructions) VALUES (%s, %s, %s)",
+                       (title, description, instructions))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('index'))
+    return render_template('add_recipe.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
